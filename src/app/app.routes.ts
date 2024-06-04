24348e51247df97de955bb214f3a2da2bfe4f7d0
KT <chan.KT@inventec.com>
2024-06-04 15:31:42 +0800

    1. 新增 router 2. routerLink 3. redirectTo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/post-list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'post-add',
+    component: PostEditComponent
+  }
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header', // 以 <app-header></app-header> 引用
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
